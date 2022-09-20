32e4a82c0956ed535f50f10f5d7659a6727aa740
Effnkid <l.bobbsemple.gmail.com>
2022-09-20 18:16:20 -0400

    fixed lineItem model

--- server/db/models/LineItem.js
+++ server/db/models/LineItem.js
@@ -2,9 +2,11 @@ const conn = require('../connection');
 const { Sequelize } = conn;
 
 const LineItem = conn.define('lineItem', {
-	type: Sequelize.INTEGER,
-	allowNull: false,
-	default: 1,
+	item: {
+		type: Sequelize.INTEGER,
+		allowNull: false,
+		default: 1,
+	},
 });
 
 module.exports = LineItem;
