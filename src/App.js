5e53f1aba218ad523a61ba74099a713e53e01851
primogill <primoledeboergill@gmail.com>
2022-09-20 19:12:29 +0200

    basic navbar

--- src/App.js
+++ src/App.js
@@ -10,12 +10,14 @@ export default function App() {
 	return (
 		<div className="App">
 			<Nav />
-			<Routes>
-				<Route index path="/cards" element={<Cards />} />
-				<Route index path="/cards/:id" element={<SingleCard />} />
-				<Route exact path="/profile" element={<Profile />} />
-				<Route exact path="/cart" element={<Cart />} />
-			</Routes>
+			<div className='rest'>
+				<Routes>
+					<Route index path="/cards" element={<Cards />} />
+					<Route index path="/cards/:id" element={<SingleCard />} />
+					<Route exact path="/profile" element={<Profile />} />
+					<Route exact path="/cart" element={<Cart />} />
+				</Routes>
+			</div>
 		</div>
 	);
 }
--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 const Nav = () => {
 	return (
-		<nav className="navBar">
-			<Link to="/cards"> Cards </Link>
-			<Link to="/profile"> profile </Link>
-			<Link to="/cart"> cart </Link>
+		<nav className="nav-bar">
+				<Link className="nav-link" to="/cards"> Cards </Link>
+				<Link className="nav-link" to="/profile"> profile </Link>
+				<Link className="nav-link" to="/cart"> cart </Link>
 		</nav>
 	);
 };
