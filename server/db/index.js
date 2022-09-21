80f63ac0753673eac801ac08ed88ea83427b3804
Effnkid <l.bobbsemple.gmail.com>
2022-09-21 12:26:02 -0400

    some cards

--- server/db/index.js
+++ server/db/index.js
@@ -15,45 +15,43 @@ Order.hasMany(LineItem);
 
 const syncAndSeed = async () => {
 	try {
-		// const getChampions = async () => {
-		// 	let { data } = await axios.get(
-		// 		'http://ddragon.leagueoflegends.com/cdn/12.17.1/data/en_US/champion.json'
-		// 	);
-		// 	let championArr = Object.values(data.data);
-		// 	return championArr;
-		// };
-		// const setRandomPrice = () => {
-		// 	return Math.floor(Math.random() * 100000) / 100;
-		// };
-		// //change to true to reseed
-		// await conn.sync({ force: true });
-		// let champions = await getChampions();
-		// let tags = await Tag.bulkCreate([
-		// 	{ name: 'Fighter' },
-		// 	{ name: 'Tank' },
-		// 	{ name: 'Mage' },
-		// 	{ name: 'Assassin' },
-		// 	{ name: 'Tank' },
-		// 	{ name: 'Support' },
-		// 	{ name: 'Mage' },
-		// 	{ name: 'Marksman' },
-		// ]);
+		const getChampions = async () => {
+			let { data } = await axios.get(
+				'http://ddragon.leagueoflegends.com/cdn/12.17.1/data/en_US/champion.json'
+			);
+			let championArr = Object.values(data.data);
+			return championArr;
+		};
+		const setRandomPrice = () => {
+			return Math.floor(Math.random() * 100000) / 100;
+		};
+		//change to true to reseed
+		await conn.sync({ force: true });
+		let champions = await getChampions();
+		let tags = await Tag.bulkCreate([
+			{ name: 'Fighter' },
+			{ name: 'Tank' },
+			{ name: 'Mage' },
+			{ name: 'Assassin' },
+			{ name: 'Tank' },
+			{ name: 'Support' },
+			{ name: 'Mage' },
+			{ name: 'Marksman' },
+		]);
 
-		// await Promise.all(
-		// 	champions.map((champion) => {
-		// 		return Product.create({
-		// 			name: champion.name,
-		// 			price: setRandomPrice(),
-		// 			qty: 100,
-		// 			descriptionBlurb: champion.blurb,
-		// 			imgAll: `http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champion.name}_0.jpg`,
-		// 			imgSingle: `http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champion.name}_0.jpg`,
-		// 			imgCart: `http://ddragon.leagueoflegends.com/cdn/12.17.1/img/champion/${champion.name}.png`,
-		// 			tag1: champion.tags[0],
-		// 			tag2: champion.tags[1] ? champion.tags[1] : null,
-		// 		});
-		// 	})
-		// );
+		champions.map(async (champion) => {
+			await Product.create({
+				name: champion.name,
+				price: setRandomPrice(),
+				qty: 100,
+				descriptionBlurb: champion.blurb,
+				imgAll: `http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champion.name.trim()}_0.jpg`,
+				imgSingle: `http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champion.name.trim()}_0.jpg`,
+				imgCart: `http://ddragon.leagueoflegends.com/cdn/12.17.1/img/champion/${champion.name.trim()}.png`,
+				tag1: champion.tags[0],
+				tag2: champion.tags[1] ? champion.tags[1] : null,
+			});
+		});
 		//use this area to sync your database
 
 		console.log(`Seeding successful!`);
--- src/features/cards/Cards.js
+++ src/features/cards/Cards.js
@@ -4,19 +4,20 @@ import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 const Cards = () => {
 	const cards = useSelector((state) => state.cards.cards);
+
 	const dispatch = useDispatch();
 
 	React.useEffect(() => {
 		dispatch(fetchCards());
 	}, [dispatch]);
-	console.log(products);
+
 	return !cards ? (
 		<div> Loading... </div>
 	) : (
-		<div>
+		<div className="cards-container">
 			{cards.map((card) => (
-				<div key={card.id}>
-					<img src={card.imgAll} alt="" />
+				<div key={card.id} className="cards">
+					<img src={card.imgAll} alt="" />;
 					<Link to={`/card/${card.id}`}>
 						<span>{card.name}</span>
 					</Link>
