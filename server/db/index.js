24f64365cd37941c7e6815fd0471e418902fa7b1
Effnkid <l.bobbsemple.gmail.com>
2022-09-21 13:29:40 -0400

    gum2

--- server/db/index.js
+++ server/db/index.js
@@ -13,17 +13,6 @@ LineItem.belongsTo(Product);
 Order.belongsTo(User);
 Order.hasMany(LineItem);
 
-const getChampions = async () => {
-	let { data } = await axios.get(
-		'http://ddragon.leagueoflegends.com/cdn/12.17.1/data/en_US/champion.json'
-	);
-	let championArr = Object.values(data.data);
-	return championArr;
-};
-const setRandomPrice = () => {
-	return Math.floor(Math.random() * 100000) / 100;
-};
-
 const syncAndSeed = async () => {
 	try {
 		const getChampions = async () => {
@@ -37,7 +26,7 @@ const syncAndSeed = async () => {
 			return Math.floor(Math.random() * 100000) / 100;
 		};
 		//change to true to reseed
-		await conn.sync({ force: false });
+		await conn.sync({ force: true });
 		let champions = await getChampions();
 		let tags = await Tag.bulkCreate([
 			{ name: 'Fighter' },
@@ -65,21 +54,6 @@ const syncAndSeed = async () => {
 		});
 		//use this area to sync your database
 
-		await Promise.all(
-			champions.map((champion) => {
-				return Product.create({
-					name: champion.name,
-					price: setRandomPrice(),
-					qty: 100,
-					descriptionBlurb: champion.blurb,
-					imgAll: `http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champion.name}_0.jpg`,
-					imgSingle: `http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champion.name}_0.jpg`,
-					imgCart: `http://ddragon.leagueoflegends.com/cdn/12.17.1/img/champion/${champion.name}.png`,
-					tag1: champion.tags[0],
-					tag2: champion.tags[1] ? champion.tags[1] : null,
-				});
-			})
-		);
 		console.log(`Seeding successful!`);
 	} catch (e) {
 		console.log(e);
