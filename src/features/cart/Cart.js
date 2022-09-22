b966a7d2863596bd8c5774cb9e299732bff10ba8
Effnkid <l.bobbsemple.gmail.com>
2022-09-22 15:13:47 -0400

    gum

--- src/features/cart/Cart.js
+++ src/features/cart/Cart.js
@@ -1,99 +1,50 @@
 import React, { useEffect, useState } from 'react';
-<<<<<<< HEAD
-import { useSelector, useDispatch } from 'react-redux';
-const Cart = () => {
-	const [qty, setQty] = useState(1);
-
-	const cart = useSelector((state) => state.cart.cart);
-	console.log(cart);
-	function addToQty() {
-		setQty(qty + 1);
-	}
-	function removeFromQty() {
-		setQty(qty - 1);
-	}
-	return (
-		<div className="cart-container">
-			<h1>Cart Items:</h1>
-			{!cart ? (
-				<h1> EMPTY CART </h1>
-			) : (
-				cart.map((item) => (
-					<div className="cart-item" key={item.id}>
-						<div className="cart-item-img">
-							<img src={item.imgCart} alt="" />
-						</div>
-						<div>
-							<h5>{item.name}</h5>
-							<h5>qty</h5>
-							<h5>price</h5>
-						</div>
-						<div className="cart-item">
-							<h5>name1</h5>
-							<div>
-								<div className="qty-button">
-									<button onClick={removeFromQty}>-</button>
-									<h5>{qty}</h5>
-									<button onClick={addToQty}>+</button>
-								</div>
-							</div>
-							<h5>$ 5,95</h5>
-						</div>
-					</div>
-				))
-			)}
-			{/* <div className="cart-item">
-				<h5>all items</h5>
-				<h5>qty: {qty}</h5>
-				<h5>$5,95</h5>
-			</div> */}
-			<button>checkout</button>
-=======
 import { UserCart } from './cartSlice';
-import {useDispatch, useSelector} from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux';
 
 const Cart = () => {
-
-	const getData = useSelector(state => state.login.user);
+	const getData = useSelector((state) => state.login.user);
 	const getCart = getData.orders;
-	const cart = getCart ? getCart.filter(item => item.isCart === true) : undefined;
+	const cart = getCart
+		? getCart.filter((item) => item.isCart === true)
+		: undefined;
 	const dispatch = useDispatch();
-	
+
 	let totalPrice = 0;
 	let totalQty = 0;
 
-	const [login,setLogin]=useState({
-		username: "sally12",
-		password: "asdf"
-	})
+	const [login, setLogin] = useState({
+		username: 'sally12',
+		password: 'asdf',
+	});
 
-    useEffect(()=> {
-        event.preventDefault()
-        dispatch(UserCart(login))
-    }, []);
+	useEffect(() => {
+		event.preventDefault();
+		dispatch(UserCart(login));
+	}, []);
 
 	return !cart ? (
 		<div>no items found</div>
 	) : (
-		<div className='rest'>
+		<div className="rest">
 			<div className="temp-cart-div">
 				<h1>{getName}'s cart</h1>
 				<div>
-					<div className='cart-item'>
+					<div className="cart-item">
 						<h5>item</h5>
 						<h5>qty</h5>
 						<h5>price</h5>
 					</div>
 					<div>
-						{cart[0].lineItems.map((cartItem)=>{
+						{cart[0].lineItems.map((cartItem) => {
 							totalQty += cartItem.quantity;
-							totalPrice += (Number(cartItem.product.price) * cartItem.quantity)
+							totalPrice += Number(cartItem.product.price) * cartItem.quantity;
 
 							return (
-								<div className='cart-item' key={cartItem.product.id}>
-									<div className='cart-img-name'>
+								<div className="cart-item" key={cartItem.product.id}>
+									<div className="cart-img-name">
 										<h5>{cartItem.product.name}</h5>
-										<img className="cart-img" src={cartItem.product.imgCart}/>
+										<img className="cart-img" src={cartItem.product.imgCart} />
 									</div>
 									<div className="qty-button">
 										{/* <button onClick={removeFromQty}>-</button> */}
@@ -102,19 +53,17 @@ const Cart = () => {
 									</div>
 									<h5>${Number(cartItem.product.price) * cartItem.quantity}</h5>
 								</div>
-
-							)
+							);
 						})}
 					</div>
 				</div>
-				<div className='cart-item'>
+				<div className="cart-item">
 					<h5>all items</h5>
 					<h5>{totalQty}</h5>
 					<h5>${totalPrice}</h5>
 				</div>
 				<button>checkout</button>
 			</div>
->>>>>>> main
 		</div>
 	);
 };
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,34 +1,6 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-<<<<<<< HEAD
-=======
-
-export const UserCart = createAsyncThunk('user/auth', async (credentials)=>{
-    try{
-        const response = await axios.post('/api/auth', credentials)
-        const token = response.data
-        window.localStorage.setItem('token',token)
-        if(token){
-            const {data:auth} = await axios.get('/api/auth',{
-                headers:{
-                    authorization:token
-                }
-            })
-            const {id} = auth
-            const {data:user} = await axios.get(`/api/auth/${id}/user`, {
-                headers:{
-                    authorization:token
-                }
-            })
-            return user
-        }else{
-            throw 'login failed bad credentials'
-        }
-    }catch(e){console.log(e)}
-})
-
->>>>>>> main
 const initialState = {
 	cart: [],
 };
@@ -38,28 +10,20 @@ const cartSlice = createSlice({
 	initialState,
 	reducers: {
 		addToCart(state, action) {
-<<<<<<< HEAD
-			state.cart.push(action.payload);
-=======
-            const productItems = state.cart.map(item => item.productId)
-            if (productItems.includes(action.payload.productId)) {
-                state.cart.map(item => {
-                    if (item.productId === action.payload.productId){
-                        item.qty += action.payload.qty;
-                    }
-                })
-            } else {
-                state.cart.push(action.payload);
-            }
->>>>>>> main
+			const productItems = state.cart.map((item) => item.productId);
+			if (productItems.includes(action.payload.productId)) {
+				state.cart.map((item) => {
+					if (item.productId === action.payload.productId) {
+						item.qty += action.payload.qty;
+					}
+				});
+			} else {
+				state.cart.push(action.payload);
+			}
 		},
 	},
 });
 
 export const { addToCart } = cartSlice.actions;
 
-<<<<<<< HEAD
 export default cartSlice.reducer;
-=======
-export default cartSlice.reducer;
->>>>>>> main
