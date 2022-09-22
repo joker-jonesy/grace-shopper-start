2473b00472d959c86262d3a90e2ce762dd63fafc
primogill <primoledeboergill@gmail.com>
2022-09-22 17:39:21 +0200

    cart pulling from state

--- src/features/cart/Cart.js
+++ src/features/cart/Cart.js
@@ -1,15 +1,34 @@
 import React, { useEffect, useState } from 'react';
+import { UserCart } from './cartSlice';
+import {useDispatch, useSelector} from 'react-redux'
 
 const Cart = () => {
-	const props = [{id: 1, name: "name1", qty: 1, price: 5099}, {id: 2, name: "name2", qty: 1, price: 3099}];
+
+	const getData = useSelector(state => state.login.user);
+	const getCart = getData.orders;
+	const cart = getCart ? getCart.filter(item => item.isCart === true) : undefined;
+	const dispatch = useDispatch();
+	
 	let totalPrice = 0;
 	let totalQty = 0;
 
-	// let totalDisplayPrice = totalPrice.toString().slice(0)
-	return (
+	const [login,setLogin]=useState({
+		username: "sally12",
+		password: "asdf"
+	})
+
+    useEffect(()=> {
+        event.preventDefault()
+        dispatch(UserCart(login))
+    }, []);
+
+
+	return !cart ? (
+		<div>no items found</div>
+	) : (
 		<div className='rest'>
 			<div className="temp-cart-div">
-				<h1>Cart Items:</h1>
+				<h1>{getName}'s cart</h1>
 				<div>
 					<div className='cart-item'>
 						<h5>item</h5>
@@ -17,34 +36,40 @@ const Cart = () => {
 						<h5>price</h5>
 					</div>
 					<div>
-						{props.map(item=> {
-							const [qty, setQty] = useState(item.qty);
-							function addToQty(){
-								setQty(qty + 1);
-							};
-							function removeFromQty(){
-								setQty(qty - 1);
-							};
-							let price = (Number(item.price) * qty ).toString().split("");
-							price.splice(price.length-2, 0, ",").join("");
+						{cart[0].lineItems.map((cartItem)=>{
+							totalQty += cartItem.quantity;
+							totalPrice += (Number(cartItem.product.price) * cartItem.quantity)
+							// Cannot use state Headers, so I need to make a reduceQuantity function like this (see jpfp):
 
-							totalPrice += (item.price * qty);
-							totalQty += qty;
+							// const handleDelete = (evnt) => {
+							// 	const campusId = campus.id
+							// 	evnt.preventDefault();
+							// 	dispatch(deleteCampus({ campusId }))
+							// }
+
+							//lets do price as a number?
+							// let price = (Number(cartItem.product.price) * qty ).toString().split("");
+							// price.splice(price.length-2, 0, ",").join("");
+
+							// totalPrice += (item.price * qty);
+							// totalQty += qty;
 
 							return (
-								<div className='cart-item' key={item.id}>
-									<h5>{item.name}</h5>
-									<div className="qty-button">
-										<button onClick={removeFromQty}>-</button>
-										<h5>{qty}</h5>
-										<button onClick={addToQty}>+</button>
+								<div className='cart-item' key={cartItem.product.id}>
+									<div className='cart-img-name'>
+										<h5>{cartItem.product.name}</h5>
+										<img className="cart-img" src={cartItem.product.imgCart}/>
 									</div>
-									<h5>${price}</h5>
+									<div className="qty-button">
+										{/* <button onClick={removeFromQty}>-</button> */}
+										<h5>{cartItem.quantity}</h5>
+										{/* <button onClick={addToQty}>+</button> */}
+									</div>
+									<h5>${Number(cartItem.product.price) * cartItem.quantity}</h5>
 								</div>
 
 							)
 						})}
-
 					</div>
 				</div>
 				<div className='cart-item'>
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,2 +1,26 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
+
+export const UserCart = createAsyncThunk('user/auth', async (credentials)=>{
+    try{
+        const response = await axios.post('/api/auth', credentials)
+        const token = response.data
+        window.localStorage.setItem('token',token)
+        if(token){
+            const {data:auth} = await axios.get('/api/auth',{
+                headers:{
+                    authorization:token
+                }
+            })
+            const {id} = auth
+            const {data:user} = await axios.get(`/api/auth/${id}/user`, {
+                headers:{
+                    authorization:token
+                }
+            })
+            return user
+        }else{
+            throw 'login failed bad credentials'
+        }
+    }catch(e){console.log(e)}
+})
