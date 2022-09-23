c400678f7daa2bbe5d3f863630b4222c1cd55ea3
Effnkid <l.bobbsemple.gmail.com>
2022-09-23 17:28:33 -0400

    gum

--- src/features/login/loginSlice.js
+++ src/features/login/loginSlice.js
@@ -13,128 +13,25 @@ const initialState = {
 	token,
 };
 
-<<<<<<< HEAD
-export const checkToken = createAsyncThunk('user/auth/token', async ()=>{
-    const token = localStorage.getItem('token')
-    if(token){
-        const {data:auth} = await axios.get('/api/auth',{
-            headers:{
-                authorization:token
-            }
-        })
-        const {id} = auth
-        const {data:user} = await axios.get(`/api/auth/${id}/user`, {
-            headers:{
-                authorization:token
-            }
-        })
-        return {user,token, loggedIn:true}
-    }else user = {}
-    return {user,token:null, loggedIn:false}
-})
+export const checkToken = createAsyncThunk('user/auth/token', async () => {
+	const token = localStorage.getItem('token');
+	if (token) {
+		const { data: auth } = await axios.get('/api/auth', {
+			headers: {
+				authorization: token,
+			},
+		});
+		const { id } = auth;
+		const { data: user } = await axios.get(`/api/auth/${id}/user`, {
+			headers: {
+				authorization: token,
+			},
+		});
+		return { user, token, loggedIn: true };
+	} else user = {};
+	return { user, token: null, loggedIn: false };
+});
 
-export const userLogin = createAsyncThunk('user/auth', async (credentials)=>{
-    try{
-        const response = await axios.post('/api/auth', credentials)
-        const token = response.data
-        window.localStorage.setItem('token',token)
-        if(token){
-            const {data:auth} = await axios.get('/api/auth',{
-                headers:{
-                    authorization:token
-                }
-            })
-            const {id} = auth
-            const {data:user} = await axios.get(`/api/auth/${id}/user`, {
-                headers:{
-                    authorization:token
-                }
-            })
-            return {user, token}
-        }else{
-            throw 'login failed bad credentials'
-        }
-    }catch(e){console.log(e)}
-})
-export const fetchUser = createAsyncThunk('user/auth/return', async (token)=>{
-    try{
-    if(token){
-        const {data:auth} = await axios.get('/api/auth', {
-            headers:{
-                authorization:token
-            }
-        })
-        const {id}= auth
-        const {data:user} = await axios.get(`/api/auth/${id}/user`,{
-            headers:{
-                authorization:token
-            }
-        })
-        return user
-    }else{throw 'update failed bad credentionals'}}
-    catch(error){console.log(error)}
-})
-
-export const loginSlice = createSlice({
-    name: 'login',
-    initialState,
-    reducers:{
-        logout: (state)=>{
-            localStorage.removeItem('token')
-            state.loading = false
-            state.user = {}
-            state.token = null
-            state.error = null
-            state.loggedIn = false
-        }
-    },
-    extraReducers(builder){
-        builder
-            .addCase(userLogin.pending, (state,action)=>{
-                state.status = 'loading'
-            })
-            .addCase(userLogin.fulfilled, (state,action)=>{
-                state.status = 'succeeded'
-                state.loggedIn = true
-                state.user = action.payload.user
-                state.token = action.payload.token
-            })
-            .addCase(userLogin.rejected, (state,action)=>{
-                state.status = 'failed'
-                state.error = action.error
-                state.loggedIn = false
-            })
-            .addCase(fetchUser.pending, (state,action)=>{
-                state.status = 'loading'
-            })
-            .addCase(fetchUser.fulfilled, (state,action)=>{
-                state.status = 'succeeded'
-                state.loggedIn = true
-                state.user = action.payload
-                state.token = state.token
-            })
-            .addCase(fetchUser.rejected, (state,action)=>{
-                state.status = 'failed'
-                state.error = action.error
-                state.loggedIn = false
-            })
-            .addCase(checkToken.pending, (state,action)=>{
-                state.status = 'loading'
-            })
-            .addCase(checkToken.fulfilled,(state,action)=>{
-                state.status = 'succeeded'
-                state.loggedIn = action.payload.loggedIn
-                state.token= action.payload.token
-                state.user = action.payload.user
-            })
-            .addCase(checkToken.rejected,(state,action)=>{
-                state.status = 'failed'
-                state.error = action.error
-                
-            })
-    }
-})
-=======
 export const userLogin = createAsyncThunk('user/auth', async (credentials) => {
 	try {
 		const response = await axios.post('/api/auth', credentials);
@@ -160,8 +57,28 @@ export const userLogin = createAsyncThunk('user/auth', async (credentials) => {
 		console.log(e);
 	}
 });
-
-export const userLogout = createAsyncThunk();
+export const fetchUser = createAsyncThunk('user/auth/return', async (token) => {
+	try {
+		if (token) {
+			const { data: auth } = await axios.get('/api/auth', {
+				headers: {
+					authorization: token,
+				},
+			});
+			const { id } = auth;
+			const { data: user } = await axios.get(`/api/auth/${id}/user`, {
+				headers: {
+					authorization: token,
+				},
+			});
+			return user;
+		} else {
+			throw 'update failed bad credentionals';
+		}
+	} catch (error) {
+		console.log(error);
+	}
+});
 
 export const loginSlice = createSlice({
 	name: 'login',
@@ -169,10 +86,11 @@ export const loginSlice = createSlice({
 	reducers: {
 		logout: (state) => {
 			localStorage.removeItem('token');
-			state.loggedIn = false;
+			state.loading = false;
 			state.user = {};
 			state.token = null;
 			state.error = null;
+			state.loggedIn = false;
 		},
 	},
 	extraReducers(builder) {
@@ -190,10 +108,36 @@ export const loginSlice = createSlice({
 				state.status = 'failed';
 				state.error = action.error;
 				state.loggedIn = false;
+			})
+			.addCase(fetchUser.pending, (state, action) => {
+				state.status = 'loading';
+			})
+			.addCase(fetchUser.fulfilled, (state, action) => {
+				state.status = 'succeeded';
+				state.loggedIn = true;
+				state.user = action.payload;
+				state.token = state.token;
+			})
+			.addCase(fetchUser.rejected, (state, action) => {
+				state.status = 'failed';
+				state.error = action.error;
+				state.loggedIn = false;
+			})
+			.addCase(checkToken.pending, (state, action) => {
+				state.status = 'loading';
+			})
+			.addCase(checkToken.fulfilled, (state, action) => {
+				state.status = 'succeeded';
+				state.loggedIn = action.payload.loggedIn;
+				state.token = action.payload.token;
+				state.user = action.payload.user;
+			})
+			.addCase(checkToken.rejected, (state, action) => {
+				state.status = 'failed';
+				state.error = action.error;
 			});
 	},
 });
->>>>>>> main
 
 export const getUser = (state) => state.user.user;
 export const { logout } = loginSlice.actions;
