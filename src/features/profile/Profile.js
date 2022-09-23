aa1d9274182847ea2baa1db8a16e34dea0ffb01a
Effnkid <l.bobbsemple.gmail.com>
2022-09-23 17:30:13 -0400

    gum1

--- src/features/profile/Profile.js
+++ src/features/profile/Profile.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 import AdminProfile from '../admin/AdminProfile';
 import Login from '../login/Login';
@@ -7,46 +7,64 @@ import { logout, checkToken } from '../login/loginSlice';
 import { Link } from 'react-router-dom';
 
 const Profile = () => {
-	const user = useSelector(state=>state.login.user)
-	useEffect(()=>{
-		dispatch(checkToken())
-	},[])
-	
-	const dispatch = useDispatch()
-<<<<<<< HEAD
-	useEffect(()=>{},[])
-	return <div>
-=======
+	const user = useSelector((state) => state.login.user);
+	useEffect(() => {
+		dispatch(checkToken());
+	}, []);
 
-	return <div className='view-profile-container'>
->>>>>>> main
-		{!user.id ? <Login/> : user.isAdmin ? <AdminProfile/> :
-		<div>
-		<h2>
-			Welcome {user.fName} {user.lName}
-		</h2>
-		<button onClick={()=>{dispatch(logout())}}>
-			Logout
-		</button>
-		<Link to='/profile/updateProfile'><h4>Update Your Profile/Password</h4></Link>
-		{user.orders.length ? <div>
-			<h4>Previous Orders:</h4>
-		<div>{user.orders.map(order=>{
-			if(!order.isCart){
-				return(
-					<div key={order.id}>
-						<p>Shipped To: {order.orderStreet} {order.orderCity} 
-						{order.orderState} {order.orderZip} {order.orderCountry} </p>
-						{order.lineItems.map(product=>
-						<div key={product.id}>{product.quantity} <img src={product.product.imgAll}  alt='' />
-						</div>)}
-					</div>
-				)
-			}
-		})}</div>
-		</div>:null}
-		</div>}
-	</div>;
+	const dispatch = useDispatch();
+
+	return (
+		<div className="view-profile-container">
+			{!user.id ? (
+				<Login />
+			) : user.isAdmin ? (
+				<AdminProfile />
+			) : (
+				<div>
+					<h2>
+						Welcome {user.fName} {user.lName}
+					</h2>
+					<button
+						onClick={() => {
+							dispatch(logout());
+						}}
+					>
+						Logout
+					</button>
+					<Link to="/profile/updateProfile">
+						<h4>Update Your Profile/Password</h4>
+					</Link>
+					{user.orders.length ? (
+						<div>
+							<h4>Previous Orders:</h4>
+							<div>
+								{user.orders.map((order) => {
+									if (!order.isCart) {
+										return (
+											<div key={order.id}>
+												<p>
+													Shipped To: {order.orderStreet} {order.orderCity}
+													{order.orderState} {order.orderZip}{' '}
+													{order.orderCountry}{' '}
+												</p>
+												{order.lineItems.map((product) => (
+													<div key={product.id}>
+														{product.quantity}{' '}
+														<img src={product.product.imgAll} alt="" />
+													</div>
+												))}
+											</div>
+										);
+									}
+								})}
+							</div>
+						</div>
+					) : null}
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default Profile;
