eae705070340c9d20e2e32374c14f22d97357eeb
Effnkid <l.bobbsemple.gmail.com>
2022-09-27 11:39:07 -0400

    gum

--- src/features/cards/singleCard.js
+++ src/features/cards/singleCard.js
@@ -28,7 +28,6 @@ const SingleCard = () => {
 
 	useEffect(() => {
 		//needs error handeling
-<<<<<<< HEAD
 		if (card.id) {
 			axios
 				.get(
@@ -37,13 +36,6 @@ const SingleCard = () => {
 				.then((response) => {
 					setCardLore(response.data.data[card.name].lore);
 				});
-=======
-		if (card.id){
-			axios.get(`http://ddragon.leagueoflegends.com/cdn/12.18.1/data/en_US/champion/${card.name}.json`
-			).then(response => {
-				setCardLore(response.data.data[card.name].lore)
-			})
->>>>>>> main
 		}
 	}, [card]);
 
@@ -53,40 +45,51 @@ const SingleCard = () => {
 		</div>
 	) : (
 		<div>
-					<div className='card-display'>
-						<img className="single-card-image" src={card.imgSingle}></img>
-						<div className="single-card-info">
-							<div className="single-card-title">{card.name}</div>
-							<div className="single-view-tags">
-								{card.tag2 ? (
-									<div className="tag-wrapper">
-										<div className="tag">{card.tag1}</div>
-										<div className="tag">{card.tag2}</div>
-									</div>
-								) : (
-									<div className="tag-wrapper">
-										<div className="tag">{card.tag1}</div>
-									</div>
-								)}
+			<div className="card-display">
+				<img className="single-card-image" src={card.imgSingle}></img>
+				<div className="single-card-info">
+					<div className="single-card-title">{card.name}</div>
+					<div className="single-view-tags">
+						{card.tag2 ? (
+							<div className="tag-wrapper">
+								<div className="tag">{card.tag1}</div>
+								<div className="tag">{card.tag2}</div>
 							</div>
-							<div className='blurb-wrapper'>
-								<div className='card-blurb'>
-									<center>Lore:</center>
-									<p>{cardLore}</p>
-								</div>
+						) : (
+							<div className="tag-wrapper">
+								<div className="tag">{card.tag1}</div>
 							</div>
-							<div className='single-card-store-info'>
-								<div className='single-card-price'>Price: ${card.price}</div>
-								<div className="card-quantity">{card.qty > 5 ? "In Stock" : (card.qty === 0 ? "Out of Stock" : `Only ${card.qty} in stock`)}</div>
-							</div>
-							<div className='cart-button-flex'>
-								<button className='single-add-to-cart-button' onClick={() =>
-							handleAddToCart({ card: card, qty: 1, price: card.price })
-						}>Add to cart</button>
-							</div>
+						)}
+					</div>
+					<div className="blurb-wrapper">
+						<div className="card-blurb">
+							<center>Lore:</center>
+							<p>{cardLore}</p>
 						</div>
 					</div>
+					<div className="single-card-store-info">
+						<div className="single-card-price">Price: ${card.price}</div>
+						<div className="card-quantity">
+							{card.qty > 5
+								? 'In Stock'
+								: card.qty === 0
+								? 'Out of Stock'
+								: `Only ${card.qty} in stock`}
+						</div>
+					</div>
+					<div className="cart-button-flex">
+						<button
+							className="single-add-to-cart-button"
+							onClick={() =>
+								handleAddToCart({ card: card, qty: 1, price: card.price })
+							}
+						>
+							Add to cart
+						</button>
+					</div>
 				</div>
+			</div>
+		</div>
 	);
 };
 
