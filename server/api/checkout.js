be70474dca3c6a82a16eddd49487af43d8e34433
Bill Munkacsy <billmunkacsy@gmail.com>
2022-09-27 17:30:39 -0400

    deploy successful

--- server/api/checkout.js
+++ server/api/checkout.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { Order, User, LineItem, Product } = require('../db');
-const stripe = require('stripe')(sk_test_51LmLfPHD1eHRXMIWHRrI5LoUgpSKe3zNS62BgW1sxhkbFstLpNXf8uIOR4AUxwBqEzh09GqbFMn28UFEPTcnJa6e00yzNlrURO);
+const stripe = require('stripe')('sk_test_51LmLfPHD1eHRXMIWHRrI5LoUgpSKe3zNS62BgW1sxhkbFstLpNXf8uIOR4AUxwBqEzh09GqbFMn28UFEPTcnJa6e00yzNlrURO');
 
 const requireToken = async (req, res, next) => {
 	try {
