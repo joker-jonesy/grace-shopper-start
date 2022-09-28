a037657638f410eb1eb1b1a44c63eed0225c2d6d
Brandon Lowe <BLoweCLE@gmail.com>
2022-09-28 14:21:36 -0500

    updated splash page

--- src/features/splash/Splash.js
+++ src/features/splash/Splash.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Splash = () => {
 
     const getArt = () => {
-        switch(Math.floor(Math.random() * 10)){
+        switch(Math.floor(Math.random() * 9)){
             case 0:
                 return 'https://res.cloudinary.com/ddqp7dojc/image/upload/v1664125349/600528_vcn7we.png';
             case 1:
@@ -13,7 +14,7 @@ const Splash = () => {
             case 3:
                 return 'https://res.cloudinary.com/ddqp7dojc/image/upload/v1664125349/wp2866676_ocdioz.jpg';
             case 4:
-                return 'https://res.cloudinary.com/ddqp7dojc/image/upload/v1664125349/3865180_zvghua.jpg';
+                return 'https://res.cloudinary.com/ddqp7dojc/image/upload/v1664125348/xPlY82_jqb1tm.jpg';
             case 5:
                 return 'https://res.cloudinary.com/ddqp7dojc/image/upload/v1664125348/be1f2fd9afa91d50a4f6723f38db331b_xaujj2.jpg';
             case 6:
@@ -22,8 +23,6 @@ const Splash = () => {
                 return 'https://res.cloudinary.com/ddqp7dojc/image/upload/v1664125348/214591_htviwg.jpg';
             case 8:
                 return 'https://res.cloudinary.com/ddqp7dojc/image/upload/v1664125348/wp2866634_sfbw2v.jpg';
-            case 9:
-                return 'https://res.cloudinary.com/ddqp7dojc/image/upload/v1664125348/xPlY82_jqb1tm.jpg';
         }
     }
 
@@ -36,6 +35,10 @@ const Splash = () => {
 
     return (
         <div className='splash-container'>
+            <div className="splash-text-div">
+                <p className="splash-text">Welcome to our League of Legends Card store. Discover your champions, read up on Lore, and checkout once you've chosen your champion!</p>
+                <Link className="splash-text" id="splash-button" to="/cards">continue</Link>
+            </div>
             <img className='splash-image' src={getArt()}/>
         </div>
     )
