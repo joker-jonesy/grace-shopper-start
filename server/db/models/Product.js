dd29c95a8d0a4a4615f6a710ff30f88fddfc2605
Bill Munkacsy <billmunkacsy@gmail.com>
2022-09-28 09:51:26 -0400

    added default image for admin create

--- server/db/models/Product.js
+++ server/db/models/Product.js
@@ -14,13 +14,16 @@ const Product = conn.define('product', {
 	},
 	imgSingle: {
 		type: Sequelize.STRING,
+		defaultValue: 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Yuumi_0.jpg'
 	},
 	imgAll: {
 		type: Sequelize.STRING,
+		defaultValue: `https://ddragon.leagueoflegends.com/cdn/img/champion/loading/Yuumi_0.jpg`
 	},
 	imgCart: {
 		type: Sequelize.STRING,
-	},
+		defaultValue: 'https://ddragon.leagueoflegends.com/cdn/12.17.1/img/champion/Yuumi.png'
+	},	
 	descriptionBlurb: {
 		type: Sequelize.TEXT,
 	},
